ddaced9c86681912de1f0db8f9facf3345eb4d54
mmacmull <54158913+mmacmull@users.noreply.github.com>
2021-01-27 13:21:12 -0300

    Update Untitled8 (1).jl

--- Untitled8 (1).jl
+++ Untitled8 (1).jl
@@ -1,290 +1,243 @@
 
-using LinearAlgebra, Plots
-using Statistics
-using Interpolations, NLsolve, Optim, Random
-using QuantEcon
-using Distributions
+using InstantiateFromURL
+# optionally add arguments to force installation: instantiate = true, precompile = true
+github_project("QuantEcon/quantecon-notebooks-julia", version = "0.8.0")
 
-#Parameter Values
-β = 0.9
-a_bar = 1
-γ = 0.2
-τ = 1.1
-N = 10
-ρ = 0.5
-σ = 1
-ξ = 1.1
-f_0 = 5
-f_1 = 4
-n_std = 1
-T = 10000
-ϵ = 4
-v=0.2
+using LinearAlgebra, Statistics
+using Parameters, QuantEcon, DataFrames, Plots, Random
 
-#Discretize fixed cost probability function:
-f_0_max = v*f_0
-f_1_max = v*f_1
-# Generate n possible values of f for the two cases
-n=5
-f0_grid = range(0, stop=f_0_max, step=(f_0_max/(n-1)))
-f1_grid = range(0, stop=f_1_max, step=(f_1_max/(n-1)))
-#Obtain probabilities:
-prob_0 = zeros(n)
-prob_1 = zeros(n)
-for i=1:n-1
-    
-            prob_0[i] = (f0_grid[i+1]/f_0_max)^(1/(1 - v))-(f0_grid[i]/f_0_max)^(1/(1 - v)) #prob fction under f0
-            prob_1[i] = (f1_grid[i+1]/f_1_max)^(1/(1 - v))-(f1_grid[i]/f_1_max)^(1/(1 - v)) #prob fction under f1
-        
+function ArellanoEconomy(;β = .953,
+                          γ = 2.,
+                          r = 0.017,
+                          ρ = 0.945,
+                          η = 0.025,
+                          θ = 0.282,
+                          ny = 21,
+                          nB = 251)
+
+    # create grids
+    Bgrid = collect(range(-.4, .4, length = nB))
+    mc = tauchen(ny, ρ, η)
+    Π = mc.p
+    ygrid = exp.(mc.state_values)
+    ydefgrid = min.(.969 * mean(ygrid), ygrid)
+
+    # define value functions
+    # notice ordered different than Python to take
+    # advantage of column major layout of Julia)
+    vf = zeros(nB, ny)
+    vd = zeros(1, ny)
+    vc = zeros(nB, ny)
+    policy = zeros(nB, ny)
+    q = ones(nB, ny) .* (1 / (1 + r))
+    defprob = zeros(nB, ny)
+
+    return (β = β, γ = γ, r = r, ρ = ρ, η = η, θ = θ, ny = ny,
+            nB = nB, ygrid = ygrid, ydefgrid = ydefgrid,
+            Bgrid = Bgrid, Π = Π, vf = vf, vd = vd, vc = vc,
+            policy = policy, q = q, defprob = defprob)
 end
-h0=zeros(length(f0_grid))
-h1=zeros(length(f1_grid))
 
-for i=1:length(f0_grid) h0[i]=f0_grid[i] end
-for i=1:length(f1_grid) h1[i]=f1_grid[i] end
+u(ae, c) = c^(1 - ae.γ) / (1 - ae.γ)
 
-f_grid=[0;h0[1:length(prob_1)-1];h1[1:length(prob_1)-1]]
-f_prob = [1;prob_0[1:length(prob_0)-1];prob_1[1:length(prob_1)-1]] #probabilities all together (i.e. dont sum up to 1)
+function one_step_update!(ae,
+                          EV,
+                          EVd,
+                          EVc)
 
+    # unpack stuff
+    @unpack β, γ, r, ρ, η, θ, ny, nB = ae
+    @unpack ygrid, ydefgrid, Bgrid, Π, vf, vd, vc, policy, q, defprob = ae
+    zero_ind = searchsortedfirst(Bgrid, 0.)
 
-#Discretize AR(1) process:
-a = QuantEcon.tauchen(N,ρ,σ,a_bar,n_std)
-#Obtain T observations of productivity
-a_t = simulate(a, T)
-p_z_i = stationary_distributions(a)
-p_z= hcat(p_z_i...)' #probabilities
-z = unique(a_t) #states
+    for iy in 1:ny
+        y = ae.ygrid[iy]
+        ydef = ae.ydefgrid[iy]
 
+        # value of being in default with income y
+        defval = u(ae, ydef) + β * (θ * EVc[zero_ind, iy] + (1-θ) * EVd[1, iy])
+        ae.vd[1, iy] = defval
 
+        for ib in 1:nB
+            B = ae.Bgrid[ib]
 
-## function that does the VFI
+            current_max = -1e14
+            pol_ind = 0
+            for ib_next=1:nB
+                c = max(y - ae.q[ib_next, iy]*Bgrid[ib_next] + B, 1e-14)
+                m = u(ae, c) + β * EV[ib_next, iy]
 
-function solve(
-
-                # grid returned by the create_grid function
-
-                z,
-
-                # model parameters
-
-                β, ϵ, ξ, τ,
-
-                # VFI tolerance (set to be tiny when running)
-
-                tolerance=10^(-6),
-
-                # max VFI iterations (to prevent the loop from running forever if something is broken), set to a large integer
-
-                iterations_max=1000
-
-            )
-
-
-
-
-
-    n = length(z)
-
-
-    ## prepare objects to store the value and policy functions
-
-    # initial V_0 guess
-
-    V = zeros(Float64, (length(z), length(f_grid)))
-    
-    # empty vector to store the policy function in:
-
-    policy_m = zeros(Float64, (length(z), length(f_grid)))
-
-
-    # initialize variables to keep tracking of iterations
-
-    iteration_counter = 0
-
-    distance = 10.0^6
-
-
-
-    # iterate!
-
-    while distance > tolerance && iteration_counter < iterations_max
-
-        # increase iteration counter by 1
-
-        iteration_counter = iteration_counter + 1
-
-        
-        # initialize an empty V_prime
-
-        
-        V_prime = zeros(Float64, (length(z), length(f_grid)))
-
-
-        # iterate over productivity (i.e. over rows)
-
-        for i=1:length(z)
-            
-            
-            # iterate over f (columns):
-            
-            for j=1:length(f_grid)
-                
-                
-            # initialize a vector to hold values of the objective function at m={0,1}
-
-            rhs_value = zeros(Float64, (length(z), 1+length(f0_grid)+length(f1_grid)))
-            
-                # compute price at a=a_i, {m-1,m}={m-1_j,m_k} and profits            
-                
-                if j<=1 #non-exporter
-                    
-                p = (1/z[i])*(ϵ/(ϵ-1))
-                π = (p^(1-ϵ)-p^(-ϵ))/z[i]
-                        
-                        else #exporter
-                        
-                p = (1/z[i])*(ϵ/(ϵ-1))*((1+ξ^(-ϵ)*τ^(-ϵ))/(1+ξ^(1-ϵ)*τ^(-ϵ)))
-                π = p^(1-ϵ)+p^(1-ϵ)*ξ^(1-ϵ)*τ^(-ϵ)-(p^(-ϵ)+((p*ξ)^(-ϵ)*τ^(-ϵ))/z[i]) 
-            
+                if m > current_max
+                    current_max = m
+                    pol_ind = ib_next
                 end
-                    
-              
-                # Calculate expected value of not exporting at t+1:
-                        
-                E_0 = sum((p_z').*V[:,1])
-                
-                        
-                            if j<=1 #non-exporter at t
-                                
-                                E_1 = sum(((p_z').*V[:,2:length(prob_0)]).*(prob_0[1:length(prob_0)-1]'))
-                                
-                                if E_0 > E_1
-                                    
-                                rhs_value[i,j] = π - f_grid[j] + β*E_0 #decides not to export at t+1
-                                
-                                policy_m[i,j] = 0
-                    
-                                
-                                else
-                                    
-                                    rhs_value[i,j] = π - f_grid[j] + β*E_1 #decides to export at t+1
-                                    
-                                    policy_m[i,j] = 1
-                                    
-                                end
-                
-              
-                            else #exporter at t
-                                
-                        
-                                E_1 = sum(((p_z').*V[:,length(prob_0):length(f_grid)]).*(prob_1'))
-                                
-                                
-                                if E_0 > E_1
-                                    
-                                rhs_value[i,j] = π - f_grid[j] + β*E_0 #decides not to export at t+1
-                                
-                                policy_m[i,j] = 0
-                                    
-                                else
-                                    
-                                rhs_value[i,j] = π - f_grid[j] + β*E_1 #decides to export at t+1
-                                
-                                policy_m[i,j] = 1
-                                    
-                                end
-                                
-                            
-                                
-                                end                
-            
-                            
 
+            end
 
-            # update v_prime at i,j with this value
-
-            V_prime[i,j] = rhs_value[i,j]
-                    
-                    
-                    
-                        end
-
-
-
+            # update value and policy functions
+            ae.vc[ib, iy] = current_max
+            ae.policy[ib, iy] = pol_ind
+            ae.vf[ib, iy] = defval > current_max ? defval : current_max
         end
+    end
+end
 
+function compute_prices!(ae)
+    # unpack parameters
+    @unpack β, γ, r, ρ, η, θ, ny, nB = ae
 
+    # create default values with a matching size
+    vd_compat = repeat(ae.vd, nB)
+    default_states = vd_compat .> ae.vc
 
-        # measure the distance between V_prime and V
+    # update default probabilities and prices
+    copyto!(ae.defprob, default_states * ae.Π')
+    copyto!(ae.q, (1 .- ae.defprob) / (1 + r))
+    return
+end
 
-        distance = maximum(abs.(V_prime.-V))
+function vfi!(ae; tol = 1e-8, maxit = 10000)
 
+    # unpack stuff
+    @unpack β, γ, r, ρ, η, θ, ny, nB = ae
+    @unpack ygrid, ydefgrid, Bgrid, Π, vf, vd, vc, policy, q, defprob = ae
+    Πt = Π'
 
+    # Iteration stuff
+    it = 0
+    dist = 10.
 
-        # update the VF guess. If we just do V=V_prime, Julia will simply point both arrays to the same object in memory, and so when start overwriting V_prime on next iteration, V will be overwritten with it. We don't want that, so actually create a full copy of V_prime, to disconnect the two objects from each other in memory
+    # allocate memory for update
+    V_upd = similar(ae.vf)
 
-        V = deepcopy(V_prime)
+    while dist > tol && it < maxit
+        it += 1
 
+        # compute expectations for this iterations
+        # (we need Π' because of order value function dimensions)
+        copyto!(V_upd, ae.vf)
+        EV = ae.vf * Πt
+        EVd = ae.vd * Πt
+        EVc = ae.vc * Πt
 
+        # update value function
+        one_step_update!(ae, EV, EVd, EVc)
 
-        if distance > tolerance
+        # update prices
+        compute_prices!(ae)
 
-            printstyled("Iteration #", iteration_counter, ". Distance: ", distance, " > ", tolerance, ". Keep going!\n", color=:red)
+        dist = maximum(abs(x - y) for (x, y) in zip(V_upd, ae.vf))
 
+        if it % 25 == 0
+            println("Finished iteration $(it) with dist of $(dist)")
+        end
+    end
+end
+
+function QuantEcon.simulate(ae,
+                            capT = 5000;
+                            y_init = mean(ae.ygrid),
+                            B_init = mean(ae.Bgrid),
+                            )
+
+    # get initial indices
+    zero_index = searchsortedfirst(ae.Bgrid, 0.)
+    y_init_ind = searchsortedfirst(ae.ygrid, y_init)
+    B_init_ind = searchsortedfirst(ae.Bgrid, B_init)
+
+    # create a QE MarkovChain
+    mc = MarkovChain(ae.Π)
+    y_sim_indices = simulate(mc, capT + 1; init = y_init_ind)
+
+    # allocate and fill output
+    y_sim_val = zeros(capT+1)
+    B_sim_val, q_sim_val = similar(y_sim_val), similar(y_sim_val)
+    B_sim_indices = fill(0, capT + 1)
+    default_status = fill(false, capT + 1)
+    B_sim_indices[1], default_status[1] = B_init_ind, false
+    y_sim_val[1], B_sim_val[1] = ae.ygrid[y_init_ind], ae.Bgrid[B_init_ind]
+
+    for t in 1:capT
+        # get today's indexes
+        yi, Bi = y_sim_indices[t], B_sim_indices[t]
+        defstat = default_status[t]
+
+        # if you are not in default
+        if !defstat
+            default_today = ae.vc[Bi, yi] < ae.vd[yi]
+
+            if default_today
+                # default values
+                default_status[t] = true
+                default_status[t + 1] = true
+                y_sim_val[t] = ae.ydefgrid[y_sim_indices[t]]
+                B_sim_indices[t + 1] = zero_index
+                B_sim_val[t+1] = 0.
+                q_sim_val[t] = ae.q[zero_index, y_sim_indices[t]]
+            else
+                default_status[t] = false
+                y_sim_val[t] = ae.ygrid[y_sim_indices[t]]
+                B_sim_indices[t + 1] = ae.policy[Bi, yi]
+                B_sim_val[t + 1] = ae.Bgrid[B_sim_indices[t + 1]]
+                q_sim_val[t] = ae.q[B_sim_indices[t + 1], y_sim_indices[t]]
+            end
+
+        # if you are in default
         else
+            B_sim_indices[t + 1] = zero_index
+            B_sim_val[t+1] = 0.
+            y_sim_val[t] = ae.ydefgrid[y_sim_indices[t]]
+            q_sim_val[t] = ae.q[zero_index, y_sim_indices[t]]
 
-            printstyled(". Iteration #", iteration_counter, ". Distance: ", distance, " <= ", tolerance, ". Ok!\n", color=:green)
-
+            # with probability θ exit default status
+            default_status[t + 1] = rand() ≥ ae.θ
         end
-
     end
 
-
-
-    # return grid, policy, and VF (this is a matrix)
-
-    return V, policy_m, z
-
-
-
+    return (y_sim_val[1:capT], B_sim_val[1:capT], q_sim_val[1:capT],
+            default_status[1:capT])
 end
 
 
 
-
-
-#tobe completed
-# run the solve() function, pass the grid you've created and all other parameters
-
-VFI = solve(
-
-                # grid returned by the create_grid function
-
-                k_grid,
-
-                # model parameters
-
-                0.98, 4, 1.2, 1.1,
-
-                # VFI tolerance (set to be tiny when running)
-
-                10^(-6),
-
-                # max VFI iterations (to prevent the loop from running forever if something is broken), set to a large integer
-
-                1000
-
-            )
+using DataFrames, Plots
+gr(fmt=:png);
 
 
 
+ae = ArellanoEconomy(β = .953,     # time discount rate
+                     γ = 2.,       # risk aversion
+                     r = 0.017,    # international interest rate
+                     ρ = .945,     # persistence in output
+                     η = 0.025,    # st dev of output shock
+                     θ = 0.282,    # prob of regaining access
+                     ny = 21,      # number of points in y grid
+                     nB = 251)     # number of points in B grid
+
+# now solve the model on the grid.
+vfi!(ae)
 
 
-## plot the results
+# create "Y High" and "Y Low" values as 5% devs from mean
+high, low = 1.05 * mean(ae.ygrid), 0.95 * mean(ae.ygrid)
+iy_high, iy_low = map(x -> searchsortedfirst(ae.ygrid, x), (high, low))
 
-#plot(VFI[3],VFI[1])
+# extract a suitable plot grid
+x = zeros(0)
+q_low = zeros(0)
+q_high = zeros(0)
+for i in 1:ae.nB
+    b = ae.Bgrid[i]
+    if -0.35 ≤ b ≤ 0  # to match fig 3 of Arellano
+        push!(x, b)
+        push!(q_low, ae.q[i, iy_low])
+        push!(q_high, ae.q[i, iy_high])
+    end
+end
 
-#plot(VFI[3],VFI[2])
-
-
+# generate plot
+plot(x, q_low, label = "Low")
+plot!(x, q_high, label = "High")
+plot!(title = "Bond price schedule q(y, B')",
+      xlabel = "B'", ylabel = "q", legend_title = "y", legend = :topleft)
 
